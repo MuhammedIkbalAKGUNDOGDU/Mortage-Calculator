da228711f04c828e630b72c76f33ef6e610e4d6c
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2024-07-17 16:52:17 +0300

    some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-import illustrationEmpty from "./assets/illustration-empty.svg" 
+import illustrationEmpty from "./assets/illustration-empty.svg";
 const App = () => {
   return (
     <div className="container">
       <div className="inner-container">
         <div className="inner-container-1">
+          <div className="inner-container-1-header">
+            <h2 className="font-weight-700">Mortgage Calculator</h2>
+            <p className="font-weight-700">Clear All</p>
+          </div>
 
+          <div>
+            <label htmlFor="mortgage-amount">Mortgage Amount</label>
+            <div className="input-container">
+              <span className="currency-symbol">£</span>
+              <input type="text" id="mortgage-amount" name="mortgage-amount" />
+            </div>
+          </div>
         </div>
         <div className="inner-container-2">
-          <img className="inner-container-2-svg"src={illustrationEmpty} alt="" />
+          <img
+            className="inner-container-2-svg"
+            src={illustrationEmpty}
+            alt=""
+          />
           <h1>Result Shown Here</h1>
-          <p>Complete the form and click "calculate repayment" to see what your monthly repayments would be</p>
+          <p>
+            Complete the form and click "calculate repayment" to see what your
+            monthly repayments would be
+          </p>
         </div>
       </div>
     </div>
