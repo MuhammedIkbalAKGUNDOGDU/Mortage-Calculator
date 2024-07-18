4c89e246b2db55354c3e67b4598f1dccc9dc6bd2
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2024-07-18 13:19:26 +0300

    all buttons and css is finshed except error css added a function a celar all button

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,131 @@
-import React from "react";
+import React, { useState } from "react";
 import illustrationEmpty from "./assets/illustration-empty.svg";
+import calculateimg from "./assets/icon-calculator.svg";
 
 import Button from "react-bootstrap/Button";
 
 const App = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = () => {
+    setIsChecked(true);
+    setIsChecked2(false);
+  };
+
+  const [isChecked2, setIsChecked2] = useState(false);
+
+  const handleChange2 = () => {
+    setIsChecked(false);
+    setIsChecked2(true);
+  };
+
+  const calculate = () => {
+    console.log("selam");
+  };
+
+  const [mortgageAmount, setMortgageAmount] = useState("");
+  const [mortgageTerm, setMortgageTerm] = useState("");
+  const [interestRate, setInterestRate] = useState("");
+
+  // Clear All işlevi
+  const clearAll = () => {
+    setMortgageAmount("");
+    setMortgageTerm("");
+    setInterestRate("");
+    setIsChecked(false);
+    setIsChecked2(false);
+  };
+
   return (
     <div className="container">
       <div className="inner-container">
         <div className="inner-container-1">
           <div className="inner-container-1-header">
             <h2 className="font-weight-700">Mortgage Calculator</h2>
-            <p className="font-weight-700">Clear All</p>
+            <p onClick={clearAll} className="font-weight-700">
+              Clear All
+            </p>
           </div>
 
           <div className="amount-container">
-            <label className="amount-label" htmlFor="mortgage-amount">
-              Mortgage Amount
-            </label>
+            <label className="amount-label">Mortgage Amount</label>
             <div className="input-container">
-              <input type="text" id="mortgage-amount" name="mortgage-amount" />
+              <input
+                type="text"
+                id="mortgage-amount"
+                name="mortgage-amount"
+                value={mortgageAmount}
+                onChange={(e) => setMortgageAmount(e.target.value)}
+              />
             </div>
           </div>
 
           <div className="term-rate-container">
             <div className="term-rate-container-inner term-rate-container-inner-2">
-              <label className="amount-label" htmlFor="mortgage-term">
-                Mortgage Term
-              </label>
+              <label className="amount-label">Mortgage Term</label>
               <div className="input-container-term">
-                <input type="text" id="mortgage-term"></input>
+                <input
+                  type="text"
+                  id="mortgage-term"
+                  value={mortgageTerm}
+                  onChange={(e) => setMortgageTerm(e.target.value)}
+                ></input>
               </div>
             </div>
 
             <div className="term-rate-container-inner">
-              <label className="amount-label" htmlFor="mortgage-term">
-                Interest Rate
-              </label>
+              <label className="amount-label">Interest Rate</label>
               <div className="input-container-term input-container-rate input-container-term-rate">
-                <input type="text" id="mortgage-term"></input>
+                <input
+                  type="text"
+                  id="mortgage-term"
+                  value={interestRate}
+                  onChange={(e) => setInterestRate(e.target.value)}
+                ></input>
               </div>
+            </div>
+          </div>
+          <div className="radiobutton-container">
+            <label className="radio-label">Mortgage Type</label>
+            <div
+              onClick={handleChange}
+              className={`font-weight-700 radio-container ${
+                isChecked ? "checked" : ""
+              }`}
+            >
+              <input
+                type="radio"
+                id="html"
+                name="fav_language"
+                value="HTML"
+                checked={isChecked}
+                onChange={handleChange}
+              />
+              <label>REPAYMENT</label>
+            </div>
+            <div
+              onClick={handleChange2}
+              className={`font-weight-700 radio-container ${
+                isChecked2 ? "checked2" : ""
+              }`}
+            >
+              <input
+                type="radio"
+                id="css"
+                name="fav_language"
+                value="CSS"
+                checked={isChecked2}
+                onChange={handleChange2}
+              />
+              <label>INTEREST ONLY</label>
+            </div>
+          </div>
+
+          <div className="button-container">
+            <div onClick={calculate} className="button-container-1">
+              <img src={calculateimg}></img>
+
+              <div className="font-weight-700">Calculate Repayments</div>
             </div>
           </div>
         </div>
